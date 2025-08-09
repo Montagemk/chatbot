c91a8ff572caee6f4b38cfed40ecf064ff9220d2
Montagemk <mtbeloh@gmail.com>
2025-08-09 01:18:51 -0300

    Delete init_db.py

--- init_db.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-
-# Importe seus modelos aqui para que o SQLAlchemy saiba quais tabelas criar
-from models import Customer, Conversation, Sale, Product, AILearningData, WhatsAppWebhook
-
-with app.app_context():
-    # Cria todas as tabelas que estão definidas nos seus modelos
-    db.create_all()
-    print("Banco de dados inicializado e tabelas criadas com sucesso!")
